c27618640d1d76a04cdd6acbcb4a862af36aa6fd
Vedant Pareek <dunefro@gmail.com>
2024-03-20 14:24:55 +0530

    Bug fix for len(azs) != len(subnets)

--- locals.tf
+++ locals.tf
@@ -1,3 +1,8 @@
+data "aws_subnet" "selected" {
+  count = length(var.private_subnets_id)
+  id    = var.private_subnets_id[count.index]
+}
+
 locals {
   tags = merge(
     {
@@ -7,4 +12,8 @@ locals {
     },
     var.tags
   )
+  subnets = merge({ for _, v in data.aws_subnet.selected : v.availability_zone => v.id... })
+  mount_targets = merge({ for k, v in local.subnets : k => {
+    subnet_id = v[0]
+  } })
 }
