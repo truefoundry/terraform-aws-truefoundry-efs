a16a6de1fa5afb8f273a0c1d0fa25b57fd480f39
Vedant Pareek <36420365+dunefro@users.noreply.github.com>
2025-08-08 15:23:51 +0530

    Revert "Update EFS to mount single subnet per az (#16)" (#17)
    This reverts commit 5726ece043cb41f5e78f9e87cc3af034579a892f.

--- output.tf
+++ output.tf
@@ -13,9 +13,3 @@ output "efs_security_group_id" {
 output "efs_role_arn" {
   value = var.create_efs_iam_role ? module.iam_assumable_role_admin[0].iam_role_arn : var.existing_efs_iam_role_arn
 }
-
-# Output to show which subnets were selected for EFS mount targets
-output "efs_selected_subnets" {
-  description = "Subnets selected for EFS mount targets (one per AZ)"
-  value       = local.efs_subnets
-}
