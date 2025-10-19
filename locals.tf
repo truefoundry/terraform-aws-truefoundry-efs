ba3f27d6f9818cee071a804ffd213a8d79c6f868
Vedant Pareek <dunefro@gmail.com>
2025-10-19 16:16:39 +0530

    Added support for CMK

--- locals.tf
+++ locals.tf
@@ -8,5 +8,5 @@ locals {
     var.tags
   )
   efs_access_policy_default_prefix = "${var.cluster_name}-access-to-efs"
-  efs_access_policy_prefix = var.efs_access_policy_prefix_enable_override ? "${var.efs_access_policy_prefix_override_name}-${local.efs_access_policy_default_prefix}" : local.efs_access_policy_default_prefix
+  efs_access_policy_prefix         = var.efs_access_policy_prefix_enable_override ? "${var.efs_access_policy_prefix_override_name}-${local.efs_access_policy_default_prefix}" : local.efs_access_policy_default_prefix
 }
